06ae710b4e69024f1eec923f0331d8bbaf58d74f
paulosarmento <paulo.cesar.sarmento@hotmail.com>
2021-12-24 19:38:32 -0300

    Recebendo eventos de bancos de dados a partir de uma inserção

---
+++ src/util/decoratorValidator.js
@@ -0,0 +1,25 @@
+const decoratorValidator = (fn, schema, argsType) => {
+  return async function (event) {
+    const data = JSON.parse(event[argsType]);
+    // abortEarly == mostrar todos os erros de uma vez
+    const { error, value } = await schema.validate(data, { abortEarly: false });
+    //isso faz alterar a instância de arguments
+    event[argsType] = value;
+    // arguments serve para pegar todos os argumentos que vieram na função
+    // e mandar para frente
+    // o apply vai retornar a função que será executada posteriormente
+    if (!error) return fn.apply(this, arguments);
+
+    return {
+      statusCode: 422, // unprocessable entity,
+      body: error.message,
+    };
+
+    // console.log("event", event);
+    // console.log("schema", schema);
+    // console.log("argsType", argsType);
+
+    // return fn.apply(this, arguments);
+  };
+};
+module.exports = decoratorValidator;
